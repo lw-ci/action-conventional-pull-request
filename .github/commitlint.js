8b453d3d5f1de7cf933659474fc75d0d3c83cddf
Layton Whiteley <layton.w@smart-host.com>
2024-09-01 20:16:10 +0200

    ci: update sample custom config

--- .github/commitlint.js
+++ .github/commitlint.js
@@ -1,5 +1,21 @@
 export default {
   rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', types],
+    'scope-enum': [2, 'always', scopes],
+    'scope-case': [2, 'always', 'lower-case'],
     'duplicate-type-subject-start': [2, 'always'],
+    'duplicate-commit-type': [2, 'always'],
   },
 };
