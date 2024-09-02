98ba3101ab2405c38fa3398166b03fac343d75a0
Layton Whiteley <layton.w@smart-host.com>
2024-09-02 08:11:17 +0200

    build: update dist folder

--- dist/helpers.js
+++ dist/helpers.js
@@ -1,15 +1,8 @@
 import * as core from '@actions/core';
-const ENUM_SPLIT_REGEX = /\n/;
-function parseNewLineList(input) {
-    return input
-        .split(ENUM_SPLIT_REGEX)
-        .map((part) => part.trim())
-        .filter(Boolean);
-}
 export function parseConfig() {
     const githubToken = process.env.GITHUB_TOKEN ?? '';
     const configFile = core.getInput('config_file');
-    const ignoreLabels = parseNewLineList(core.getInput('ignore_labels'));
+    const ignoreLabels = core.getMultilineInput(core.getInput('ignore_labels'));
     const baseConfig = core.getInput('base_config') || '@commitlint/config-conventional';
     return {
         configFile,
